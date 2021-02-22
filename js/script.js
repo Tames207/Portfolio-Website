5f63e7f0262cd7fc72a9488c3505cc7b7beeecd9
Tyler Ames <ames.tyler@gmail.com>
2021-02-21 18:38:25 -0700

    adding the mobile menu function to the coding.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 console.log("Hello World, Welcome to the collection of my work");
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
