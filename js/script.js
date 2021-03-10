0b818ddd8e06fc6aceead2e16c059fd0631b3e4f
Tyler Ames <ames.tyler@gmail.com>
2021-03-09 19:23:11 -0700

    making the edits from the W3 validator and the plugins on Atom

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-console.log("Hello World, Welcome to the collection of my work");
+console.log("Hello World, Welcome to the collection of my work")
 
 function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
+  var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle') {
-    x.className += ' responsive';
+    x.className += ' responsive'
   } else {
-    x.className = 'navtoggle';
+    x.className = 'navtoggle'
   }
 }
